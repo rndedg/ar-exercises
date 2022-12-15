df72d4df3543f1ee99c719beb1e3b3c382749dbb
Gord Letkeman <rndedg@gmail.com>
2022-12-15 18:41:57 +0000

    Add exercise_6 along with modified store.rb and employee.rb

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Dave", last_name: "Williams", hourly_rate: 50)
+
+@store2.employees.create(first_name: "Karen", last_name: "Jones", hourly_rate: 58)
+@store2.employees.create(first_name: "Scott", last_name: "Johnson", hourly_rate: 64)
