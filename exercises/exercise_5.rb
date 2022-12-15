e78dc41d5fda27811358a80870b3ea0a4ddeb09d
Gord Letkeman <rndedg@gmail.com>
2022-12-15 18:16:37 +0000

    Add exercise_5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,6 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+p Store.sum('annual_revenue')
+p Store.average(:annual_revenue).to_i
+p Store.where('annual_revenue >= ?', 1000000).count
